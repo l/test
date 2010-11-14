3a00ec9a014096959bb37101c4f3b8c0f7c7f56b
l <l@github.com>
2010-11-14 20:39:26 +0900

    add direct feed subscription mode for pushfeed.pl and swich uri option to web option

--- perl/googlereader_pushfeed.pl
+++ perl/googlereader_pushfeed.pl
@@ -4,7 +4,8 @@
 # by "AYANOKOUZI, Ryuunosuke" <i38w7i3@yahoo.co.jp>
 # under GNU General Public License v3.
 #
-# $ perl googlereader_pushfeed.pl --config ~/.google.json --uri 'http://example.com/'
+# $ perl googlereader_pushfeed.pl --config ~/.google.json --web 'http://example.com/'
+# $ perl googlereader_pushfeed.pl --config ~/.google.json --feed 'http://example.com/feed.xml'
 #
 
 use strict;
@@ -18,7 +19,7 @@ use Web::Scraper;
 use WebService::Google::Reader;
 
 my %opts = ();
-GetOptions(\%opts, 'config=s', 'uri=s');
+GetOptions(\%opts, 'config=s', 'web=s', 'feed=s');
 
 my $io = IO::File->new();
 $io->open($opts{config}, 'r') or die $!;
@@ -26,29 +27,51 @@ my $config = decode_json(join '', $io->getlines);
 $io->close;
 
 my $reader = WebService::Google::Reader->new(%$config);
-my $uri = URI->new($opts{uri});
-my $scraper = scraper {
-	process '/html/head/link', 'link[]' => {
-		type => '@type',
-		href => '@href',
-	}
-};
 
-my $result = $scraper->scrape($uri);
-print Dumper $result;
+if (defined $opts{web}) {
+	&web($reader, $opts{web});
+} elsif (defined $opts{feed}) {
+	&feed($reader, $opts{feed});
+} else {
+	print "ERROR: --web or --feed option is needed.";
+}
+exit;
 
-foreach (@{$result->{link}}) {
-	if (defined $_->{type}) {
-		my $type = $_->{type};
-		if ($type eq 'application/rss+xml' ||
-				$type eq 'application/rdf+xml' ||
-				$type eq 'application/atom+xml') {
-			my $res = $reader->edit_feed($_->{href}, tag => 'pushfeed', subscribe => 1);
-			print Dumper $_, "\n";
-			print Dumper $reader;
-			print Dumper $res;
+sub web
+{
+	my $reader = shift;
+	my $uri = URI->new(shift);
+	my $scraper = scraper {
+		process '/html/head/link', 'link[]' => {
+			type => '@type',
+			     href => '@href',
+		}
+	};
+
+	my $result = $scraper->scrape($uri);
+	print Dumper $result;
+
+	foreach (@{$result->{link}}) {
+		if (defined $_->{type}) {
+			my $type = $_->{type};
+			if ($type eq 'application/rss+xml' ||
+					$type eq 'application/rdf+xml' ||
+					$type eq 'application/atom+xml') {
+				my $res = $reader->edit_feed($_->{href}, tag => 'pushfeed', subscribe => 1);
+				print Dumper $_, "\n";
+				print Dumper $reader;
+				print Dumper $res;
+			}
 		}
 	}
 }
-exit;
+
+sub feed
+{
+	my $reader = shift;
+	my $uri = URI->new(shift);
+	my $res = $reader->edit_feed($uri, tag => 'pushfeed', subscribe => 1);
+	print Dumper $reader;
+	print Dumper $res;
+}
 __END__
