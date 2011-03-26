078e960282ff00185a06ef54aafe8b5e7b3ea8e9
l <l@github.com>
2011-03-26 14:38:20 +0900

    install pipe mode.

--- perl/googlereader_pushfeed.pl
+++ perl/googlereader_pushfeed.pl
@@ -6,6 +6,7 @@
 #
 # $ perl googlereader_pushfeed.pl --config ~/.google.json --web 'http://example.com/'
 # $ perl googlereader_pushfeed.pl --config ~/.google.json --feed 'http://example.com/feed.xml'
+# $ perl googlereader_pushfeed.pl --config ~/.google.json --tag test_tag --pipe --feed 1 < feed.txt
 #
 
 use strict;
@@ -15,31 +16,73 @@ use Getopt::Long;
 use IO::File;
 use JSON;
 use URI;
+use LWP::UserAgent;
 use Web::Scraper;
 use WebService::Google::Reader;
 
+#print Dumper @ARGV;
 my %opts = ();
-GetOptions(\%opts, 'config=s', 'web=s', 'feed=s');
+GetOptions(\%opts, 'config=s', 'tag=s',  'web=s', 'feed=s', 'pipe');
 
 my $io = IO::File->new();
 $io->open($opts{config}, 'r') or die $!;
 my $config = decode_json(join '', $io->getlines);
 $io->close;
 
+$config->{debug} = 1;
+
 my $reader = WebService::Google::Reader->new(%$config);
 
-if (defined $opts{web}) {
-	&web($reader, $opts{web});
+if (!defined $opts{tag} || $opts{tag} eq '') {
+	 $opts{tag} = 'pushfeed';
+}
+#print Dumper %opts;
+
+if (defined $opts{pipe}) {
+	if (defined $opts{web}) {
+		&pipe($reader, $opts{tag}, \&web);
+	} elsif (defined $opts{feed}) {
+		&pipe($reader, $opts{tag}, \&feed);
+	}
+} elsif (defined $opts{web}) {
+	web($reader, $opts{tag}, $opts{web});
 } elsif (defined $opts{feed}) {
-	&feed($reader, $opts{feed});
+	feed($reader, $opts{tag}, $opts{feed});
 } else {
-	print "ERROR: --web or --feed option is needed.";
+	print "ERROR: --web or --feed or --pipe option is needed.";
 }
 exit;
+
+sub pipe
+{
+	my $reader = shift;
+	my $tag = shift;
+	my $func = shift;
+	my $uri = [];
+	my $io = IO::File->new;
+	$io->fdopen(fileno(STDIN), "r");
+	while(my $line = $io->getline) {
+		chomp $line;
+		print $line, "\n";
+#		my $uri = URI->new($line);
+		push @{$uri}, $line;
+		if (@{$uri} > 30) {
+			&$func($reader, $tag, $uri);
+			$uri = [];
+		}
+	}
+	$io->close;
+	if (@{$uri} > 0) {
+		&$func($reader, $tag, $uri);
+		$uri = [];
+	}
+	return;
+}
 
 sub web
 {
 	my $reader = shift;
+	my $tag = shift;
 	my $uri = URI->new(shift);
 	my $scraper = scraper {
 		process '/html/head/link', 'link[]' => {
@@ -48,7 +91,19 @@ sub web
 		}
 	};
 
-	my $result = $scraper->scrape($uri);
+	my $ua = new LWP::UserAgent();
+	$ua->agent('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; ja-JP-mac; rv:1.9.0.6) Gecko/2009011912 Firefox/3.0.6 GTB5');
+	$ua->max_size(1000000);
+
+	$scraper->user_agent($ua);
+
+	my $result;# = $scraper->scrape($uri);
+	eval {$result = $scraper->scrape($uri)};
+	if ($@) {
+		warn $@;
+		return 1;
+	}
+
 	print Dumper $result;
 
 	foreach (@{$result->{link}}) {
@@ -56,10 +111,11 @@ sub web
 			my $type = $_->{type};
 			if ($type eq 'application/rss+xml' ||
 					$type eq 'application/rdf+xml' ||
+# foaf is also rdfformat
 					$type eq 'application/atom+xml') {
-				my $res = $reader->edit_feed($_->{href}, tag => 'pushfeed', subscribe => 1);
-				print Dumper $_, "\n";
-				print Dumper $reader;
+				my $res = $reader->edit_feed($_->{href}, tag => $tag, subscribe => 1);
+#				print Dumper $_, "\n";
+#				print Dumper $reader;
 				print Dumper $res;
 			}
 		}
@@ -69,9 +125,15 @@ sub web
 sub feed
 {
 	my $reader = shift;
-	my $uri = URI->new(shift);
-	my $res = $reader->edit_feed($uri, tag => 'pushfeed', subscribe => 1);
-	print Dumper $reader;
+	my $tag = shift;
+	my $uri = shift;
+#	my $uri = URI->new(shift);
+	my $res = $reader->edit_feed($uri, tag => $tag, subscribe => 1);
 	print Dumper $res;
+	if (!$res) {
+		print Dumper $reader;
+		warn $uri;
+	}
+	return;
 }
 __END__
