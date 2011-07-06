9bbf40f2b19c85b5769ce11de26f4ab8a26a0176
l <l@github.com>
2011-07-06 16:26:44 +0900

    problem 6

---
+++ project_euler/006/006.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -l
+use warnings;
+use strict;
+
+main();
+
+sub main
+{
+	my $N = 100;
+	my $result = 0;
+	my $sum = 0;
+	my $i = 0;
+	for ($i = 1; $i <= $N; $i++) {
+		$sum += ($i * $i);
+	}
+	$result = (($N * ($N + 1)) / 2);
+	$result *= $result;
+	$result = $result - $sum;
+	print $result;
+	return 0;
+}
+
+exit;
+__END__
